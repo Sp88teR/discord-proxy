e9863886107fd9b0e499d0591b9c7ecdefaeb677
Sp88teR <mahanshafiei44@gmail.com>
2025-04-06 03:16:17 +0330

    fix : use template string for targeturl

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/webhooks", async (req, res) => {
-  const targetUrl = 'https://discord.com${req.originalUrl}';
+  const targetUrl = `https://discord.com${req.originalUrl}`;
 
   try {
     const discordRes = await fetch(targetUrl, {
